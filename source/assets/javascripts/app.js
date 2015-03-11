081d56c6781aa627704ac3727921724bbf9b0c59
DLavin23 <daniel.e.lavin@gmail.com>
2015-03-10 22:31:46 -0500

    more WIP

--- source/assets/javascripts/app.js
+++ source/assets/javascripts/app.js
@@ -2,23 +2,13 @@ document.addEventListener("DOMContentLoaded", function() {
   console.log('loaded vanilla');
 
   var navButton  = document.querySelector('.btn-menu'),
-      navMenu     = document.querySelector('.nav'),
+      navMenu    = document.querySelector('.nav'),
       tabButton  = document.querySelector('.tab-btn'),
       tabItem    = document.querySelector('.tab-item'),
       tabContent = document.querySelector('.tab-content');
 
   navButton.onclick = function() {
-    // document.body.classList.toggle('is-open');
     this.classList.toggle('active');
     navMenu.classList.toggle('is-open');
   };
-
 });
-
-// angular.module('app', [function() {
-// 	console.log('hello, world!');
-// }])
-
-// .controller('AppCtrl', ['$scope', function($scope){
-//   console.log('App controller');
-// }]);
